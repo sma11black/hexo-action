ccfeb2bc558f04f503d15c60c32da6fc1d65e978
司马黑 <653014496@qq.com>
2020-01-09 22:12:35 +0800

    Uncomment hexo command. Modify output.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ set -e
 git config --global user.name "$INPUT_USER_NAME"
 git config --global user.email "$INPUT_USER_EMAIL"
 
-# sh -c "hexo $INPUT_CMD"
+sh -c "hexo $INPUT_CMD"
 
-echo ::set-output name=notify::$INPUT_CMD
+echo ::set-output name=notify::"$INPUT_CMD complate."
