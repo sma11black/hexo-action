9b7f6717dc8f1a0811446da5e1bae24b7fe19199
司马黑 <653014496@qq.com>
2020-01-09 20:28:30 +0800

    Fix "IMPUT". Comment "hexo $*"  cmd for test.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-git config --global user.name "$IMPUT_USER_NAME"
+git config --global user.name "$INPUT_USER_NAME"
 git config --global user.email "$INPUT_USER_EMAIL"
 
-sh -c "hexo $*"
+# sh -c "hexo $*"
 
 echo ::set-output name=notify::$*
