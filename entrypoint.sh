aa15bfc8593acbe104810d365afbbb87ef911a32
司马黑 <653014496@qq.com>
2020-01-09 21:37:03 +0800

    Fix inputs: cmd.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ set -e
 git config --global user.name "$INPUT_USER_NAME"
 git config --global user.email "$INPUT_USER_EMAIL"
 
-# sh -c "hexo $cmd"
+# sh -c "hexo $INPUT_CMD"
 
-echo ::set-output name=notify::$cmd
+echo ::set-output name=notify::$INPUT_CMD
