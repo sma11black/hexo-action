7ca039e9dff1e14f9e6699159dd122f270314003
司马黑 <653014496@qq.com>
2020-01-09 21:27:11 +0800

    Add hexo command as inputs.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ set -e
 git config --global user.name "$INPUT_USER_NAME"
 git config --global user.email "$INPUT_USER_EMAIL"
 
-# sh -c "hexo $*"
+# sh -c "hexo $cmd"
 
-echo ::set-output name=notify::$*
+echo ::set-output name=notify::$cmd
