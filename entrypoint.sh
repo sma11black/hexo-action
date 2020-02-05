91f5add671c9d07d2f331d62d9b8880e7dd72870
司马黑 <653014496@qq.com>
2020-02-05 18:42:54 +0800

    Add ssh setup.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,20 @@
 
 set -e
 
+# setup ssh-private-key
+mkdir -p /root/.ssh/
+echo "$INPUT_DEPLOY_KEY" > /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
+ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
+
+# setup deploy git account
 git config --global user.name "$INPUT_USER_NAME"
 git config --global user.email "$INPUT_USER_EMAIL"
 
-npm install hexo-deployer-git --save
+# install hexo env
+npm install hexo-cli -g
 npm install
+# npm install hexo-deployer-git --save
 
 sh -c "hexo $INPUT_CMD"
 
