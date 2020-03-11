5c3d4a438fa345d0c6158b8cc980150035001190
Mr. X <xaoxuu@gmail.com>
2020-03-11 16:44:59 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,7 @@ git config --global user.email "$INPUT_USER_EMAIL"
 # install hexo env
 npm install hexo-cli -g
 npm install hexo-deployer-git --save
+npm install hexo --save
 
 # deployment
 hexo g -d
