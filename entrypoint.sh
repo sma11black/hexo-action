7046bcb6ac2d303b53d12ad5ed877912cde389e7
sma11black <smallblack@outlook.com>
2020-04-20 18:02:02 +0800

    Modify commit message to github action name.

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,6 +19,6 @@ npm install hexo-deployer-git --save
 git clone https://github.com/$GITHUB_ACTOR/$GITHUB_ACTOR.github.io.git .deploy_git
 
 # deployment
-hexo g -d -m "$GITHUB_WORKFLOW"
+hexo g -d -m "$GITHUB_ACTION"
 
 echo ::set-output name=notify::"Deploy complate."
