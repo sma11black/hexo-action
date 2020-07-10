1603cd946cf93d907236737507c2656b1000e964
sma11black <smallblack@outlook.com>
2020-07-10 18:38:19 +0800

    Debug: variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,8 @@ npm install hexo-deployer-git --save
 
 # pull original publish repo
 repo=$(grep 'repo:' _config.yml)
-page_account=${${repo##*/}%.github.io*}
+repo=${repo##*/}
+page_account=${repo%.github.io*}
 ls
 echo $page_account
 git clone https://github.com/$page_account/$page_account.github.io.git .deploy_git
