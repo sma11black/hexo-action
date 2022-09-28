f39bfef667b9cd945670c934a033e881412ead68
Mark Lin <mark.lin@ttfb-lab.com>
2022-09-28 19:11:44 +0800

    npm install hexo

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,7 @@ git config --global user.email "$INPUT_USER_EMAIL"
 # install hexo env
 npm install hexo-cli -g
 npm install hexo-deployer-git --save
+npm install hexo --save
 
 # deployment
 if [ "$INPUT_COMMIT_MSG" = "none" ]
